75ad3ecf2c67028a31ffc437380636141b89827e
Jose Andres <andresjosehr@gmail.com>
2022-11-01 10:21:10 -0400

    Cambios realizados

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
 		app: 'directory',
-		api: 'https://projects.andresjosehr.com/noraruoti/api',
-		assets: 'https://projects.andresjosehr.com/noraruoti/',
+		api: 'https://projects.andresjosehr.com/api-inmobiliaria/api',
+		assets: 'https://projects.andresjosehr.com/api-inmobiliaria/',
 };
