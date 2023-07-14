931ddcbc9cf126be9934a24f9f6e449f0cc60ae0
José Andrés <joseandreshernandezross@gmail.com>
2023-07-13 21:31:35 -0400

    Cambios

--- src/app/modules/public/landing/landing.module.ts
+++ src/app/modules/public/landing/landing.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing.component';
 import { RouterModule } from '@angular/router';
 import { landingRoutes } from './home.routing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
 	declarations: [LandingComponent],
-	imports: [CommonModule, RouterModule.forChild(landingRoutes)],
+	imports: [
+		CommonModule,
+		RouterModule.forChild(landingRoutes),
+		FormsModule,
+		ReactiveFormsModule
+	],
 })
 export class LandingModule {}
