b4960b05952bd5b94de3ecd45e16d42c6e070068
Esthefanía López <yubeylopezcenteno@gmail.com>
2023-07-13 22:42:02 -0400

    Cambios en el hero

--- src/app/modules/public/landing/landing.module.ts
+++ src/app/modules/public/landing/landing.module.ts
@@ -4,14 +4,11 @@ import { LandingComponent } from './landing.component';
 import { RouterModule } from '@angular/router';
 import { landingRoutes } from './home.routing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
 	declarations: [LandingComponent],
-	imports: [
-		CommonModule,
-		RouterModule.forChild(landingRoutes),
-		FormsModule,
-		ReactiveFormsModule
-	],
+	imports: [CommonModule, RouterModule.forChild(landingRoutes), FormsModule, ReactiveFormsModule, MatIconModule, MatChipsModule],
 })
 export class LandingModule {}
